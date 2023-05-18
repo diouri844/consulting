9b9a73ad38c819a4878caca52c116e693e8dbaf7
Chopen <75852177+diouri844@users.noreply.github.com>
2023-05-18 16:14:01 +0000

    update router init

--- src/init/init.routes.ts
+++ src/init/init.routes.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express';
 import { Controller } from '../shared';
+import { ConsultingController } from '../features/consulting';
+
 const URL_PREFIX = '/api/v1';
 export default (app: Express): void => {
-  const controllers: Controller[] = [];
+  const controllers: Controller[] = [new ConsultingController()];
 
   controllers.forEach(controller => {
     console.log(URL_PREFIX + controller.path + controller.route);
