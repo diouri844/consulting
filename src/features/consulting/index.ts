2258e5af21319f613bcfae0dfed0dad1ae2d8ffa
Chopen <75852177+diouri844@users.noreply.github.com>
2023-05-18 16:14:23 +0000

    update consulting entry point

---
+++ src/features/consulting/index.ts
@@ -0,0 +1,4 @@
+export { default as ConsultingController } from './consulting.controller';
+export { default as ConsultingServices } from './consulting.services';
+export { default as Consulting } from './consulting.model';
+export { default as IConsultant } from './consulting.interface';
